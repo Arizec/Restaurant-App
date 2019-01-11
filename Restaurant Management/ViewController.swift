e98a3822786364a47a04bc7a0a1dac38a25633f4
Gabrielle Chan <salufie@gmail.com>
2019-01-11 17:22:31 +1100

    init app

---
+++ Restaurant Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant Management
+//
+//  Created by Gabrielle Chandrasaputra on 11/1/19.
+//  Copyright © 2019 Arizec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
